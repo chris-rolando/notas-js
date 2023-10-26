fc02dae55c40498fdb138ec5800c3c0dcdfa9e1e
chris-rolando <chris.rolando@outlook.com>
2023-10-26 17:56:55 -0600

    editando ejemplo: area del triangulo + reparando bugs

--- code/js/area-triangulo/areaTriangulo.js
+++ code/js/area-triangulo/areaTriangulo.js
@@ -1,9 +1,11 @@
 function compute(base, height) {
-    return `El área es: ${((base*height)/2).toFixed(2)}`; 
-  }
-  
-  function calculateArea() {
-    const base = parseInt(document.getElementById("base-request").value);
-    const height = parseInt(document.getElementById("height-request").value);
-    document.getElementById("result").textContent = compute(base, height);
-  }
+  return `El área es: ${((base * height) / 2).toFixed(2)}`;
+}
+
+function calculateArea() {
+  // obtener datos
+  const base = parseInt(document.getElementById("base-request").value);
+  const height = parseInt(document.getElementById("height-request").value);
+  // calcular() e imprimir el resultado
+  document.getElementById("result").innerHTML = compute(base, height);
+}
